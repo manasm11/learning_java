aae9252628db18478b526cb778051e1e67db759b
Manas Mishra <manas.m22@gmail.com>
2021-07-29 20:45:40 +0530

    Learned about compiling, testing and packaging projects using mvn commands

---
+++ 52.creating_first_maven_project_using_archtype/calculator-app/src/main/java/mishra/manas/Addition.java
@@ -0,0 +1,6 @@
+package mishra.manas;
+public class Addition{
+    public Double add(double a, double b) {
+        return a+b;
+    }
+}
--- 52.creating_first_maven_project_using_archtype/calculator-app/src/main/java/mishra/manas/App.java
+++ 52.creating_first_maven_project_using_archtype/calculator-app/src/main/java/mishra/manas/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Addition addition = new Addition();
+        System.out.println( "1 + 1 = " + addition.add(1, 1) );
     }
+
 }
