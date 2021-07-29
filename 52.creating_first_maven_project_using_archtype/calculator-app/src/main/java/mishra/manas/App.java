7d22c10c20efb22650cba63512fbf84c3f503c23
Manas Mishra <manas.m22@gmail.com>
2021-07-29 20:01:17 +0530

    Created basic project and notes for that

---
+++ 52.creating_first_maven_project_using_archtype/calculator-app/src/main/java/mishra/manas/App.java
@@ -0,0 +1,13 @@
+package mishra.manas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
