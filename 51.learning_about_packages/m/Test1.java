97cb3b645e8a27db5d3d48db289df6eff13047a5
Manas Mishra <manas.m22@gmail.com>
2021-07-29 19:20:53 +0530

    Starting to learn about maven

---
+++ 51.learning_about_packages/m/Test1.java
@@ -0,0 +1,7 @@
+package m;
+
+public class Test1 {
+	public Test1() {
+		System.out.println("Created Test1");
+	}
+}
---
+++ 51.learning_about_packages/m/a/Test2.java
@@ -0,0 +1,13 @@
+package m.a;
+
+import m.Test1;
+
+public class Test2 {
+	public static void main(String[] args){
+		new Test1();
+		System.out.println("HEALLO");
+	}
+	public Test2() {
+		System.out.println("Created Test2");
+	}
+}
