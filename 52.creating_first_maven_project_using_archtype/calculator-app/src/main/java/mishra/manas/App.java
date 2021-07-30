4f709ad2ad06027714360769dbdc3d173b48e992
Manas Mishra <manas.m22@gmail.com>
2021-07-30 15:54:08 +0530

    Completed course on maven

--- 52.creating_first_maven_project_using_archtype/calculator-app/src/main/java/mishra/manas/App.java
+++ 52.creating_first_maven_project_using_archtype/calculator-app/src/main/java/mishra/manas/App.java
@@ -1,5 +1,7 @@
 package mishra.manas;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 /**
  * Hello world!
  *
@@ -10,6 +12,8 @@ public class App
     {
         Addition addition = new Addition();
         System.out.println( "1 + 1 = " + addition.add(1, 1) );
+        double n = NumberUtils.toDouble("12321");
+        System.out.println(n);
     }
 
 }
